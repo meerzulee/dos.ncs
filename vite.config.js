cc031243a8dcb4148e0f234260efc1c4f1756900
Meerzulee <meerzulee@gmail.com>
2021-06-29 12:57:49 +0600

    Remove compression (failed)

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,13 @@ import path from 'path'
 
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
-import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(), 
-    compress({
-      extensions: ['png'],
-      svgo: false,
-      pngquant: true
-    })
+   
 
   ],
   resolve: {
