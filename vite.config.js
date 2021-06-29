6031954fd3ae58a3a96c2f2ce9713532c9cc4d26
Meerzulee <meerzulee@gmail.com>
2021-06-29 12:49:58 +0600

    Add compression

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,18 @@ import path from 'path'
 
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
-
+import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(), 
-   
+    compress({
+      extensions: ['png'],
+      svgo: false,
+      pngquant: true
+    })
 
   ],
   resolve: {
